d3f50e832bc30dc98c7f12bde58bc775675ae998
xiangxiang818 <navipin.hongp@mail.kmutt.ac.th>
2026-03-30 13:06:19 +0700

    818-[Add] ModalCreateRest

--- src/features/admin/ModalRestaurantForm.tsx
+++ src/features/admin/ModalRestaurantForm.tsx
@@ -1,9 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faStore } from "@fortawesome/free-solid-svg-icons"
-// import { useState } from 'react'
+import { useState } from 'react'
 
 
 function ModalRestaurantForm({ onClose } : {onClose : ()=> void}) {
+    const [preview, setPreview] = useState<string | null>(null)
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => { //get element from input type and use event of react
+        const file = e.target.files?.[0]
+        if (file) {
+            setPreview(URL.createObjectURL(file))
+        }
+    }
 
     return (
         <div className="fixed inset-0 z-99 bg-black/60 flex justify-center items-center"
@@ -30,6 +37,40 @@ function ModalRestaurantForm({ onClose } : {onClose : ()=> void}) {
                     </div>
                 </form>
 
+                <form>
+                    <label className="relative flex flex-col items-center justify-center w-full mt-6 p-5 rounded-md border-2 border-dashed border-(--color-brand-secondary)">
+                        {preview ? (
+                            <img src={preview} alt="Preview" className='box-border h-60'/>
+                        ) : (
+                            <span className='text-(--color-brand-secondary) text-lg md:text-xl h-40 md:h-60 flex justify-center items-center'>Click to add image</span>
+                        )}
+
+                        <div className=''>
+                            <input type="file"
+                            className='hidden cursor-pointer'
+                            accept='image/*'
+                            onChange={handleFileChange}
+                            />
+                        </div>
+                    </label>
+                    {preview && (
+                        <button className='border border-(--color-brand-primary)
+                        px-4 py-1 rounded-full mt-5 text-(--color-brand-primary)
+                        font-bold cursor-pointer hover:text-(--color-text-primary)
+                        duration-300 bg-(' onClick={() => setPreview(null)}>Remove Image</button>
+                    )}
+                </form>
+                <div className='flex flex-row mt-5 md:mt-7 mb-5 md:mb-7 gap-5 float-end'>
+                    <button className="border border-(--color-brand-primary) text-(--color-brand-primary) 
+                    text-sm md:text-lg px-[clamp(9px,2.5vw,18px)] py-[clamp(3px,2.5vw,6px)] rounded-full 
+                    bg-(--color-brand-primary)/20 hover:text-(--color-text-primary) hover:bg-(--color-brand-primary)
+                    duration-300 cursor-pointer">Cencel</button>
+
+                    <button className="border border-(--color-state-success) text-(--color-state-success)
+                    text-sm md:text-lg px-[clamp(9px,2.5vw,18px)] py-[clamp(3px,2.5vw,6px)] rounded-full 
+                    bg-(--color-state-success)/20 hover:text-(--color-text-primary) hover:bg-(--color-state-success)
+                    duration-300 cursor-pointer">Create</button>
+                </div>
             </div>
         </div>
     )
