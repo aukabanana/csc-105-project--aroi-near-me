2070d3aa13eeb50e504b890eff56e023f28aebc8
aukabanana <hope25501@gmail.com>
2026-04-27 05:26:01 +0700

    822 - [Add] GET / Welcome endpoint test

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import 'dotenv/config';
+import morgan from 'morgan';
+
+const app = express();
+const PORT = process.env.PORT;
+if (!PORT) throw new Error(`PORT is missing in your env file`);
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.listen(PORT, () => {
+    console.log(`Server Is Running On http://localhost:${PORT}`);
+});
+
+app.get('/', (_req, res:Response) => {
+    res.send('Connected, Welcome to BackEnd jaaaa 👾👾')
+})
