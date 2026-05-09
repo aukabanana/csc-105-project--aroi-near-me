396dc5ee36dec3e085b81b2a92ff8fcc56e7e788
aukabanana <hope25501@gmail.com>
2026-05-10 02:32:59 +0700

    822 - [update] add auto reloading page

--- frontend/src/features/admin/ModalRestaurantForm.tsx
+++ frontend/src/features/admin/ModalRestaurantForm.tsx
@@ -69,6 +69,7 @@ function ModalRestaurantForm({
 
             onSuccess?.()
             onClose()
+            window.location.reload()
         } catch (err) {
             setError(getErrorMessage(err))
         } finally {
